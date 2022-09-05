be22c6137cde541e8ddf72223c599ef1eb6046c6
Swapneel Gosavi <swapneel.gosavithoughtworks.com@Swapneels-MacBook-Pro.local>
2022-09-05 11:21:42 +0530

    Initial commit

---
+++ src/main/java/com/kodedge/springdemoapplication/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.kodedge.springdemoapplication;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path="data")
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
---
+++ src/test/java/com/kodedge/springdemoapplication/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodedge.springdemoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
